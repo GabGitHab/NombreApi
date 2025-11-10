cce94613a13d8a2d55bfbc94b9d6417e6a8952a5
Gabriel Larrosa <gabriell.7195@gmail.com>
2025-11-10 20:35:33 -0300

    Inicio

---
+++ NombreApi/Controllers/GabrielPostaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GabrielPostaController : ControllerBase
+{
+    public readonly HttpClient httpClient;
+
+    public GabrielPostaController(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+        httpClient.BaseAddress = new Uri("");
+    }
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var respuesta = await httpClient.GetAsync("http://localhost:521/api/NachoPosta");
+        if (respuesta.IsSuccessStatusCode)
+        {
+            var data = await respuesta.Content.ReadAsStringAsync();
+            Console.WriteLine("Por aqui paso la posta de Gabriel");
+        }
+
+        return StatusCode((int)respuesta.StatusCode, "");
+    }
+}
